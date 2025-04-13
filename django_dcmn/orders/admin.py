bea1e91a0e0c3b9fb78a26c2627a8b273df9aa6a
Vlad G <127021567+Umojan@users.noreply.github.com>
2025-04-13 18:17:34 +0600

    admin

--- django_dcmn/orders/admin.py
+++ django_dcmn/orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import FbiApostilleOrder, OrderFile
 
-# Register your models here.
+
+class OrderFileInline(admin.TabularInline):
+    model = OrderFile
+    extra = 0
+
+
+@admin.register(FbiApostilleOrder)
+class FbiApostilleOrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'package', 'count', 'shipping_option', 'total_price', 'is_paid', 'created_at')
+    list_filter = ('package', 'shipping_option', 'is_paid', 'created_at')
+    search_fields = ('name', 'email', 'country_name', 'address')
+    inlines = [OrderFileInline]
+
+
+@admin.register(OrderFile)
+class OrderFileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'file', 'uploaded_at')
+    search_fields = ('order__name',)
