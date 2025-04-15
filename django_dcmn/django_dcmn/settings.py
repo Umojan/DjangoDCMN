10ba33cb2c89194830e67dae4c2673d17b0ab733
Vlad G <127021567+Umojan@users.noreply.github.com>
2025-04-15 17:44:34 +0600

    CSRF

--- django_dcmn/django_dcmn/settings.py
+++ django_dcmn/django_dcmn/settings.py
@@ -132,3 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = config('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media'))
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://djangodcmn-production.up.railway.app",
+]
