2e5922a1795aa847a13ffa260a9a249df7ce312c
Vlad G <127021567+Umojan@users.noreply.github.com>
2025-06-13 02:33:44 +0600

    + embassy model

--- django_dcmn/orders/urls.py
+++ django_dcmn/orders/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import (
     CreateFbiOrderView,
     CreateMarriageOrderView,
+    CreateEmbassyOrderView,
+
     FbiOptionsView,
     CreateStripeSessionView,
 
@@ -15,6 +17,7 @@ from .views import (
 urlpatterns = [
     path('fbi/create-order/', CreateFbiOrderView.as_view(), name='fbi_create_order'),
     path('marriage/create-order/', CreateMarriageOrderView.as_view(), name='marriage_create_order'),
+    path('embassy/create-order/', CreateEmbassyOrderView.as_view(), name='create-embassy-order'),
 
     path('fbi/options/', FbiOptionsView.as_view(), name='fbi_options'),
     path("create-stripe-session/", CreateStripeSessionView.as_view(), name="create_stripe_session"),
