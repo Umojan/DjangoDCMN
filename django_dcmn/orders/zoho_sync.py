bb07a1ddcdd38b24d672e88ca8ce266ed9cfde2c
Vlad G <127021567+Umojan@users.noreply.github.com>
2025-06-21 16:37:12 +0600

    + zoho_sync.py optimized

--- django_dcmn/orders/zoho_sync.py
+++ django_dcmn/orders/zoho_sync.py
@@ -29,6 +29,45 @@ def get_access_token(force_refresh=False):
     return token
 
 
+def sync_order_to_zoho(order, module_name, data_payload, attach_files=True):
+    for attempt in range(2):
+        access_token = get_access_token(force_refresh=(attempt == 1))
+        headers = {
+            "Authorization": f"Zoho-oauthtoken {access_token}",
+            "Content-Type": "application/json"
+        }
+        resp = requests.post(f"{ZOHO_API_DOMAIN}/crm/v2/{module_name}", headers=headers, json=data_payload)
+        resp_data = resp.json()
+        print(f"Create {module_name} deal:", resp_data)
+        try:
+            record_id = resp_data['data'][0]['details']['id']
+            break
+        except Exception as e:
+            print(f"{module_name} order creation ERROR:", e)
+            if attempt == 1:
+                return False
+
+    if attach_files:
+        file_urls = [settings.BASE_URL + fa.file.url for fa in order.file_attachments.all()]
+        for url in file_urls:
+            file_response = requests.get(url)
+            file_response.raise_for_status()
+            filename = url.split('/')[-1]
+            files = {
+                'file': (filename, file_response.content)
+            }
+            attach_url = f'{ZOHO_API_DOMAIN}/crm/v2/{module_name}/{record_id}/Attachments'
+            attach_headers = {
+                'Authorization': f'Zoho-oauthtoken {access_token}'
+            }
+            response = requests.post(attach_url, headers=attach_headers, files=files)
+            print(f'Attach "{filename}":', response.status_code, response.text)
+
+    order.zoho_synced = True
+    order.save(update_fields=['zoho_synced'])
+    return True
+
+
 def sync_fbi_order_to_zoho(order: FbiApostilleOrder):
     zoho_module = 'Deals'
     data = {
@@ -52,42 +91,7 @@ def sync_fbi_order_to_zoho(order: FbiApostilleOrder):
             }
         ]
     }
-
-    for attempt in range(2):
-        access_token = get_access_token(force_refresh=(attempt == 1))
-        headers = {
-            "Authorization": f"Zoho-oauthtoken {access_token}",
-            "Content-Type": "application/json"
-        }
-        resp = requests.post(f"{ZOHO_API_DOMAIN}/crm/v2/{zoho_module}", headers=headers, json=data)
-        resp_data = resp.json()
-        print("Create deal:", resp_data)
-        try:
-            record_id = resp_data['data'][0]['details']['id']
-            break
-        except Exception as e:
-            print("Order creation ERROR:", e)
-            if attempt == 1:
-                return False
-
-    file_urls = [settings.BASE_URL + fa.file.url for fa in order.file_attachments.all()]
-    for url in file_urls:
-        file_response = requests.get(url)
-        file_response.raise_for_status()
-        filename = url.split('/')[-1]
-        files = {
-            'file': (filename, file_response.content)
-        }
-        attach_url = f'{ZOHO_API_DOMAIN}/crm/v2/{zoho_module}/{record_id}/Attachments'
-        attach_headers = {
-            'Authorization': f'Zoho-oauthtoken {access_token}'
-        }
-        response = requests.post(attach_url, headers=attach_headers, files=files)
-        print(f'Attach "{filename}":', response.status_code, response.text)
-
-    order.zoho_synced = True
-    order.save(update_fields=['zoho_synced'])
-    return True
+    return sync_order_to_zoho(order, zoho_module, data, attach_files=True)
 
 
 def sync_embassy_order_to_zoho(order: EmbassyLegalizationOrder):
@@ -111,42 +115,7 @@ def sync_embassy_order_to_zoho(order: EmbassyLegalizationOrder):
             }
         ]
     }
-
-    for attempt in range(2):
-        access_token = get_access_token(force_refresh=(attempt == 1))
-        headers = {
-            "Authorization": f"Zoho-oauthtoken {access_token}",
-            "Content-Type": "application/json"
-        }
-        resp = requests.post(f"{ZOHO_API_DOMAIN}/crm/v2/{zoho_module}", headers=headers, json=data)
-        resp_data = resp.json()
-        print("Create embassy deal:", resp_data)
-        try:
-            record_id = resp_data['data'][0]['details']['id']
-            break
-        except Exception as e:
-            print("Embassy order creation ERROR:", e)
-            if attempt == 1:
-                return False
-
-    file_urls = [settings.BASE_URL + fa.file.url for fa in order.file_attachments.all()]
-    for url in file_urls:
-        file_response = requests.get(url)
-        file_response.raise_for_status()
-        filename = url.split('/')[-1]
-        files = {
-            'file': (filename, file_response.content)
-        }
-        attach_url = f'{ZOHO_API_DOMAIN}/crm/v2/{zoho_module}/{record_id}/Attachments'
-        attach_headers = {
-            'Authorization': f'Zoho-oauthtoken {access_token}'
-        }
-        response = requests.post(attach_url, headers=attach_headers, files=files)
-        print(f'Attach "{filename}":', response.status_code, response.text)
-
-    order.zoho_synced = True
-    order.save(update_fields=['zoho_synced'])
-    return True
+    return sync_order_to_zoho(order, zoho_module, data, attach_files=True)
 
 
 def sync_translation_order_to_zoho(order: TranslationOrder):
@@ -165,42 +134,7 @@ def sync_translation_order_to_zoho(order: TranslationOrder):
             }
         ]
     }
-
-    for attempt in range(2):
-        access_token = get_access_token(force_refresh=(attempt == 1))
-        headers = {
-            "Authorization": f"Zoho-oauthtoken {access_token}",
-            "Content-Type": "application/json"
-        }
-        resp = requests.post(f"{ZOHO_API_DOMAIN}/crm/v2/{zoho_module}", headers=headers, json=data)
-        resp_data = resp.json()
-        print("Create translation deal:", resp_data)
-        try:
-            record_id = resp_data['data'][0]['details']['id']
-            break
-        except Exception as e:
-            print("Translation order creation ERROR:", e)
-            if attempt == 1:
-                return False
-
-    file_urls = [settings.BASE_URL + fa.file.url for fa in order.file_attachments.all()]
-    for url in file_urls:
-        file_response = requests.get(url)
-        file_response.raise_for_status()
-        filename = url.split('/')[-1]
-        files = {
-            'file': (filename, file_response.content)
-        }
-        attach_url = f'{ZOHO_API_DOMAIN}/crm/v2/{zoho_module}/{record_id}/Attachments'
-        attach_headers = {
-            'Authorization': f'Zoho-oauthtoken {access_token}'
-        }
-        response = requests.post(attach_url, headers=attach_headers, files=files)
-        print(f'Attach "{filename}":', response.status_code, response.text)
-
-    order.zoho_synced = True
-    order.save(update_fields=['zoho_synced'])
-    return True
+    return sync_order_to_zoho(order, zoho_module, data, attach_files=True)
 
 
 def sync_apostille_order_to_zoho(order: ApostilleOrder):
@@ -222,26 +156,4 @@ def sync_apostille_order_to_zoho(order: ApostilleOrder):
             }
         ]
     }
-
-    access_token = get_access_token()
-    headers = {
-        "Authorization": f"Zoho-oauthtoken {access_token}",
-        "Content-Type": "application/json"
-    }
-
-    resp = requests.post(
-        f"{ZOHO_API_DOMAIN}/crm/v2/{module}",
-        headers=headers,
-        json=data
-    )
-    resp_data = resp.json()
-    print("Create apostille deal:", resp_data)
-
-    try:
-        record_id = resp_data['data'][0]['details']['id']
-        order.zoho_synced = True
-        order.save(update_fields=["zoho_synced"])
-        return True
-    except Exception as e:
-        print("Apostille order creation ERROR:", e)
-        return False
+    return sync_order_to_zoho(order, module, data, attach_files=False)
